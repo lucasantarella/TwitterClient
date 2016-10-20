fb75929bfd1848bed4f581c1a626f561aa2ef8c6
Luca Santarella <luca.santarella@gmail.com>
2016-10-20 15:31:51 -0400

    20161020 Update
    * Added an AsyncTask
    * Method Tracing
    * Manifest Permissions

--- app/src/main/java/org/oratoryprep/techclub/twitterclient/MainActivity.java
+++ app/src/main/java/org/oratoryprep/techclub/twitterclient/MainActivity.java
@@ -1,7 +1,10 @@
 package org.oratoryprep.techclub.twitterclient;
 
+import android.os.AsyncTask;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +16,36 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText("HI TECH CLUB!");
+
+        AsyncTask<Void, Void, Void> asyncTask = new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected Void doInBackground(Void... params) {
+
+                Log.d("Testing", "Testing ASyncTask!");
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+
+            }
+        };
+
+        Debug.startMethodTracing("/sdcard/trace.trace");
+
+        asyncTask.execute();
+
+        Debug.stopMethodTracing();
+
+
     }
+
+
 }
